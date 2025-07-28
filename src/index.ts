2a73885a525727be8c59c51e86be3186787f75b6
Leonardo Gurgel <leo@gurgel.io>
2025-07-28 11:10:13 -0300

    ignore config files on project root

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ export default function config(...configs: InfiniteDepthConfigWithExtends[]) {
       languageOptions: {
         parserOptions: {
           projectService: {
-            allowDefaultProject: ["*.{js,cjs,mjs}"],
+            allowDefaultProject: ["*.config.{js,cjs,mjs,ts,cts,mts}"],
           },
         },
       },
