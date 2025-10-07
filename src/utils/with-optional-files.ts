58e37d1bf6679a076bec30d6c89bd3d994458f2e
Leonardo Gurgel <leo@gurgel.io>
2025-10-06 23:44:07 -0300

    chore: bump packages

--- src/utils/with-optional-files.ts
+++ src/utils/with-optional-files.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "eslint/config";
 export default function withOptionalFiles(
   configs: Linter.Config[],
   files: string[] | undefined,
-) {
+): Linter.Config[] {
   if (files != null) return defineConfig({ files, extends: configs });
   return configs;
 }
